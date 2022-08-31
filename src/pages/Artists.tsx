78605c4c97519588334055cf1411e014baaf5ebf
EmreMertA <emremakdag55@gmail.com>
2022-08-31 22:22:38 +0300

    Sidebar

---
+++ src/pages/Artists.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const Artists = (props: Props) => {
+  return <div>Artists</div>;
+};
+
+export default Artists;
---
+++ src/pages/Explore.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const Explore = (props: Props) => {
+  return <div>Explore</div>;
+};
+
+export default Explore;
